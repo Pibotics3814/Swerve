90243e9424edb592881112c9f6025aeb05086788
Eragon3650 <eragon3650@gmail.com>
2022-12-01 15:19:14 -0500

    Update to ensure compatibility gyroswervedrive-ah

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -7,6 +7,8 @@ package frc.robot;
 import frc.robot.subsystems.SwerveModule;
 
 public final class Constants {
+	public static double HALF_PI = Math.PI / 2.0;
+
 	public static double driveDirection = 1.0;
 	public static double JOYSTICK_DEADBAND = 0.2; // was .05
 	public static double TRIGGER_DEADBAND = 0.2;
