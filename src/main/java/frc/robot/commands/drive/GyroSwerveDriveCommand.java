88b43d944fbaca05c23796146272a60a929b3a48
Eragon3650 <eragon3650@gmail.com>
2022-12-07 16:17:12 -0500

    Updated for joystick input - ah

--- src/main/java/frc/robot/commands/drive/GyroSwerveDriveCommand.java
+++ src/main/java/frc/robot/commands/drive/GyroSwerveDriveCommand.java
@@ -30,9 +30,6 @@ public class GyroSwerveDriveCommand extends CommandBase {
     @Override
     public void execute() {
        m_gyroSwerveDrive.gyroDrive( dX.getAsDouble(), dY.getAsDouble(), dZ.getAsDouble(), Math.toRadians( m_gyro.getAngle() ) );
-       SmartDashboard.putNumber( "Module 2 encoder", Constants.swerveMod[1].getSteerAngle() );
-       SmartDashboard.putNumber( "Module 3 encoder", Constants.swerveMod[2].getSteerAngle() );
-       SmartDashboard.putNumber( "Module 4 encoder", Constants.swerveMod[3].getSteerAngle() );
     }
 
   
