e4259863c5b040280dfc9b02fcf34f9d135a95d2
Eragon3650 <eragon3650@gmail.com>
2022-12-09 16:24:51 -0500

    formatting update - ah

--- src/main/java/frc/robot/commands/drive/GyroSwerveDriveCommand.java
+++ src/main/java/frc/robot/commands/drive/GyroSwerveDriveCommand.java
@@ -10,45 +10,45 @@ import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import frc.robot.subsystems.GyroSwerveDrive;
 
 public class GyroSwerveDriveCommand extends CommandBase {
-    DoubleSupplier dX, dY, dZ;
-    double driveX, driveY, driveZ;
-    double rawX, rawY, rawZ;
-    ADIS16470_IMU m_gyro;
-    GyroSwerveDrive m_gyroSwerveDrive;
-    private static double DEADZONE_MAP_DIVISOR = 1.0 / (1.0 - Constants.JOYSTICK_DEADZONE);
+  DoubleSupplier dX, dY, dZ;
+  double driveX, driveY, driveZ;
+  double rawX, rawY, rawZ;
+  ADIS16470_IMU m_gyro;
+  GyroSwerveDrive m_gyroSwerveDrive;
+  private static double DEADZONE_MAP_DIVISOR = 1.0 / (1.0 - Constants.JOYSTICK_DEADZONE);
 
-    public GyroSwerveDriveCommand(DoubleSupplier stick_x, DoubleSupplier stick_y, DoubleSupplier stick_z, ADIS16470_IMU imu, GyroSwerveDrive gyroSwerveDrive) {
-        dX = stick_x;
-        dY = stick_y;
-        dZ = stick_z;
-        m_gyro = imu;
-        m_gyroSwerveDrive = gyroSwerveDrive;
-        addRequirements( m_gyroSwerveDrive );
-    }
+  public GyroSwerveDriveCommand(DoubleSupplier stick_x, DoubleSupplier stick_y, DoubleSupplier stick_z, ADIS16470_IMU imu, GyroSwerveDrive gyroSwerveDrive) {
+      dX = stick_x;
+      dY = stick_y;
+      dZ = stick_z;
+      m_gyro = imu;
+      m_gyroSwerveDrive = gyroSwerveDrive;
+      addRequirements( m_gyroSwerveDrive );
+  }
 
-    @Override
-    public void initialize() {
-    }
+  @Override
+  public void initialize() {
+  }
 
-    @Override
-    public void execute() {
-      rawX = dX.getAsDouble();
-      driveX = (Math.abs(rawX) - Constants.JOYSTICK_DEADZONE) * DEADZONE_MAP_DIVISOR;
-      if(driveX <= 0.0) driveX = 0.0;
-      else if(rawX <= 0.0) driveX = -driveX;
+  @Override
+  public void execute() {
+    rawX = dX.getAsDouble();
+    driveX = (Math.abs(rawX) - Constants.JOYSTICK_DEADZONE) * DEADZONE_MAP_DIVISOR;
+    if(driveX <= 0.0) driveX = 0.0;
+    else if(rawX <= 0.0) driveX = -driveX;
 
-      rawY = dY.getAsDouble();
-      driveY = (Math.abs(rawY) - Constants.JOYSTICK_DEADZONE) * DEADZONE_MAP_DIVISOR;
-      if(driveY <= 0.0) driveY = 0.0;
-      else if(rawY <= 0.0) driveY = -driveY;
+    rawY = dY.getAsDouble();
+    driveY = (Math.abs(rawY) - Constants.JOYSTICK_DEADZONE) * DEADZONE_MAP_DIVISOR;
+    if(driveY <= 0.0) driveY = 0.0;
+    else if(rawY <= 0.0) driveY = -driveY;
 
-      rawZ = dZ.getAsDouble();
-      driveZ = (Math.abs(rawZ) - Constants.JOYSTICK_DEADZONE) * DEADZONE_MAP_DIVISOR;
-      if(driveZ <= 0.0) driveZ = 0.0;
-      else if(rawZ <= 0.0) driveZ = -driveZ;
+    rawZ = dZ.getAsDouble();
+    driveZ = (Math.abs(rawZ) - Constants.JOYSTICK_DEADZONE) * DEADZONE_MAP_DIVISOR;
+    if(driveZ <= 0.0) driveZ = 0.0;
+    else if(rawZ <= 0.0) driveZ = -driveZ;
 
-      m_gyroSwerveDrive.gyroDrive( driveX, driveY, driveZ, Math.toRadians( m_gyro.getAngle() ) );
-    }
+    m_gyroSwerveDrive.gyroDrive( driveX, driveY, driveZ, Math.toRadians( m_gyro.getAngle() ) );
+  }
 
   
   @Override
